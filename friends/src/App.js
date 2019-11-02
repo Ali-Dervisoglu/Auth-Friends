1ceb06685fdff728d536d4aef37c17f0bfe106c5
Ali Dervisoglu <derya.dervisoglu@gmail.com>
2019-11-01 18:15:45 -0700

    protectedRoute added

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './App.css';
 import Signin from "./components/Signin"
 import { BrowserRouter, Link, Route } from "react-router-dom"
+import { getToken } from "./utils/getToken"
+import FriendsList from "./components/FriendsList";
+import ProtectedRoute from "./ProtectedRoute"
 
 function App() {
+  const signedIn = getToken();
+
   return (
     <BrowserRouter>
       <nav>
         <Link to="/"> Home </Link>
-        <Link to="/signin"> Sign In </Link>
+        {!signedIn && <Link to="/signin"> Sign In </Link>}
+        <Link to="/friendslist"> Friends List </Link>
       </nav>
       <Route exact path="/signin" component={Signin} />
-      </BrowserRouter>
+      <ProtectedRoute exact path="/friendslist" component={FriendsList} />
+    </BrowserRouter>
   );
-  }
+}
 
-  export default App;
+export default App;
